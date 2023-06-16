0b0c2a6d8483e618ba5c8d510cbfcbe15ea780ff
danovd <danovski_dimitar@abv.bg>
2023-06-16 11:45:33 +0300

    encapsulation task1 Sort by name and age

---
+++ _05_Encapsulation_Lab_01_SortByNameAndAge/Person.java
@@ -0,0 +1,30 @@
+package _05_Encapsulation_Lab_01_SortByNameAndAge;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %d years old.", this.getFirstName(), this.getLastName(), this.getAge());
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
